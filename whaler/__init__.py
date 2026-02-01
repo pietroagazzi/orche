00fb9fecc3b3c0448d3378771b51a6712415c03e
Pietro Agazzi <pietro.agazzi@prconsulting.eu>
2026-02-01 15:11:18 +0100

    chore(release): bump version to 0.3.0

--- whaler/__init__.py
+++ whaler/__init__.py
@@ -1,7 +1,9 @@
+from importlib.metadata import version
+
 from . import builtin
 from .logger import setup_logger
 from .stack import Stack
 
 __all__ = ["builtin", "setup_logger", "Stack"]
 
-__version__ = "0.2.0"
+__version__ = version("orche")
