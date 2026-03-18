3c50f3a5536cdfb91866fca2af246e13af257e50
Pietro Agazzi <contact@agazzipietro.it>
2026-03-18 19:46:47 +0100

    chore: bump version to 0.4.2

--- orche/__init__.py
+++ orche/__init__.py
@@ -1,12 +1,7 @@
-from importlib.metadata import PackageNotFoundError, version
-
 from . import builtin
 from .logger import setup_logger
 from .stack import BuiltinCommandType, Stack
 
 __all__ = ["builtin", "setup_logger", "BuiltinCommandType", "Stack"]
 
-try:
-    __version__ = version("orche")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
+__version__ = "0.4.2"
